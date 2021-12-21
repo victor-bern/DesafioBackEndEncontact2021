36dbbf38a6d9e4b6d1ae5e5a9d301d97efbebadd
Victor Bernardes <dartinjs@gmail.com>
2021-12-21 17:58:01 -0300

    contact domains created

---
+++ TesteBackendEnContact/Core/Domain/ContactBook/Contact/Contact.cs
@@ -0,0 +1,13 @@
+﻿using TesteBackendEnContact.Core.Interface.ContactBook.Contact;
+
+namespace TesteBackendEnContact.Core.Domain.ContactBook.Contact
+{
+    public class Contact : IContact
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string Phone { get; set; }
+        public int CompanyId { get; set; }
+        public int ContactBookId { get; set; }
+    }
+}
---
+++ TesteBackendEnContact/Core/Interface/ContactBook/Contact/IContact.cs
@@ -0,0 +1,11 @@
+﻿namespace TesteBackendEnContact.Core.Interface.ContactBook.Contact
+{
+    public interface IContact
+    {
+        int Id { get; set; }
+        int Name { get; set; }
+        string Phone { get; set; }
+        int CompanyId { get; set; }
+        int ContactBookId { get; set; }
+    }
+}
